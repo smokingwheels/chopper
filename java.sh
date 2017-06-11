ae7f319ca85be7d836b819dde0d1871535c9abd3
smokingwheels <smokingwheels@users.noreply.github.com>
2017-06-12 06:20:21 +0800

    Create java.sh

--- java.sh
+++ java.sh
@@ -1,5 +1,7 @@
 echo deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main > /etc/apt/sources.list.d/java-8-debian.list
 echo deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main >> /etc/apt/sources.list.d/java-8-debian.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-sudo apt-get update
+apt-get update
+echo
+echo Please run java1.sh
 
