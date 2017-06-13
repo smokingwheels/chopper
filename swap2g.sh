dc174fe94d068968f1ce19c21f1bd78f99d77aea
smokingwheels <smokingwheels@users.noreply.github.com>
2017-06-14 07:37:31 +0800

    2 GB swap file

---
+++ swap2g.sh
@@ -0,0 +1,8 @@
+cd /var
+touch swap.img
+chmod 600 swap.img
+dd if=/dev/zero of=/var/swap.img bs=2048k count=1000
+mkswap /var/swap.img
+swapon /var/swap.img
+echo "/var/swap.img    none    swap    sw    0    0" >> /etc/fstab
+
